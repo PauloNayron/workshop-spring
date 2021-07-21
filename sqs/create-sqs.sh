7f518a61ba56b009f011630722977d9854dbb381
PauloNayron <paulonayron.s@hotmail.com>
2021-07-21 00:56:13 -0300

    refactor: muda permissão de create-sqs

--- sqs/create-sqs.sh
+++ sqs/create-sqs.sh
@@ -5,7 +5,7 @@
 #aws --endpoint-url=http://localhost:4576 sqs list-queues
 
 # envia mensagem para a fila
-#aws --endpoint-url=http://localhost:4576 sqs send-message --queue-url http://localhost:4576/000000000000/create-user-sqs --message-body '{"name": "Paulo Nayron","email": "paulo.nayron@jaya.tech"}'
+aws --endpoint-url=http://localhost:4576 sqs send-message --queue-url http://localhost:4576/000000000000/create-user-sqs --message-body '{"name": "Paulo Nayron","email": "paulo.nayron@jaya.tech"}'
 
 # consume a fila
 #aws --endpoint-url=http://localhost:4576 sqs receive-message --queue-url http://localhost:4566/000000000000/playground-sqs
